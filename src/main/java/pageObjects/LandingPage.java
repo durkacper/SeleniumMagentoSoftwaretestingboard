4e029c2820bca33408c5036698e347002fc52712
Kacper <37781519+durkacper@users.noreply.github.com>
2024-06-10 12:52:33 +0200

    Constructor update

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -11,7 +11,6 @@ public class LandingPage extends BasePage {
 
     public LandingPage(WebDriver driver){
         super(driver);
-        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
